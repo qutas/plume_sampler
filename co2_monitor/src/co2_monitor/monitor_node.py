6da9909077f3be6ec14a9488b92cdbad4f61bde4
Kye Morton <pryre.dev@outlook.com>
2017-05-09 13:08:23 +0000

    CO2 sampler implemented

--- co2_monitor/src/co2_monitor/monitor_node.py
+++ co2_monitor/src/co2_monitor/monitor_node.py
@@ -7,31 +7,54 @@ from std_msgs.msg import Bool
 
 class CO2Monitor():
 	def __init__(self):
-		# Set up the publisher
-		self.pub_reading = rospy.Publisher(rospy.get_param('~topic_reading', 'reading'), Float64, queue_size=10)
-		self.pub_trigger = rospy.Publisher(rospy.get_param('~topic_trigger', 'trigger'), Bool, queue_size=10)
+		# Set up the publishers
+		self.pub_filtered = rospy.Publisher(rospy.get_name() + '/' + rospy.get_param('~topic_filtered', '~reading/filtered'), Float64, queue_size=10)
+		self.pub_raw = rospy.Publisher(rospy.get_name() + '/' + rospy.get_param('~topic_raw', 'reading/raw'), Float64, queue_size=10)
+		self.pub_trigger = rospy.Publisher(rospy.get_name() + '/' + rospy.get_param('~topic_trigger', 'trigger'), Bool, queue_size=10)
 
 		port_name = rospy.get_param('~port', '/dev/ttyUSB0')
-		port_buad = rospy.get_param('~buad', '115200')
+		port_baud = rospy.get_param('~buad', '9600')
+		self.trigger_value = rospy.get_param('~trigger_value', 800)
 
 		try:
-			self.ser = serial.Serial(port_name, port_baud, timeout=0)  # open serial port
-			rospy.loginfo("Serial connected at: " % port_name)
+			self.ser = serial.Serial(port_name, port_baud)  # open serial port
+			rospy.loginfo("Serial connected at: %s" % port_name)
+			rospy.loginfo("CO2 monitor running")
 
 		except serial.serialutil.SerialException as e:
 			rospy.loginfo(e)
-			rospy.shutdown()
+			rospy.signal_shutdown(e)
 
-		rospy.loginfo("CO2 monitor running")
-
-	def shutdown_plugin(self):
-		ser.close()             # close port
+	def shutdown(self):
+		self.ser.close()             # close port
 
 	def run(self):
-		rospy.loginfo( ser.readline() )
+		try:
+			str = self.ser.readline()
+			nums = [int(s) for s in str.split() if s.isdigit()]
+			#rospy.loginfo(nums)
+			if len(nums) == 2:
+				msg_raw_out = Float64()
+				msg_filtered_out = Float64()
+				msg_trigger_out = Bool()
 
-		#msg_out = Empty()
-		#self.pub_ping.publish(msg_out)
+				msg_filtered_out.data = nums[0]
+				msg_raw_out.data = nums[1]
+
+				if nums[0] > self.trigger_value:
+					msg_trigger_out.data = True
+				else:
+					msg_trigger_out.data = False
+
+				self.pub_raw.publish(msg_raw_out)
+				self.pub_filtered.publish(msg_filtered_out)
+				self.pub_trigger.publish(msg_trigger_out)
+
+			else:
+				rospy.loginfo("Error parsing data!")
 
 
+		except serial.serialutil.SerialException as e:
+			#rospy.loginfo(e)
+			self.shutdown()
 
